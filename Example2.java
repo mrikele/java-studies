48eb57e2e1656aee0e1594dcb2574551f66ba8de
Maria Rikele <mariaarikele@gmail.com>
2022-10-01 22:26:23 -0300

    novas alterações

---
+++ Example2.java
@@ -0,0 +1,13 @@
+class Example2 {
+    public static void main(String args[]){
+        int var1;
+        int var2;
+
+        var1 = 1024;
+        System.out.println("varl contains " + var1);
+
+        var2 = var1 / 2;
+        System.out.print("var contains var/2: " + var2);
+        System.out.println(var2);
+    }
+}
